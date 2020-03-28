f3c8b327903c839901265fdb8612cc8a9964e1b8
Jos Badpak <31701470+DennisGHUA@users.noreply.github.com>
2020-03-28 16:35:19 +0100

    Mooie launcher met naam verandering

--- src/View/UserView.java
+++ src/View/UserView.java
@@ -12,10 +12,13 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import javax.swing.*;
 
 
 public class UserView extends View {
@@ -28,23 +31,20 @@ public class UserView extends View {
     }
 
     // Define buttons
-    Button loginSubmit = new Button("Login");
-    Button logout = new Button("Logout");
+    Button btnChangeName = new Button("");
 
     // Define text fields
-    TextField loginName = new TextField();
+    TextField usernameEdit = new TextField();
 
     // Default window resolution
     int windowWidth = 1280;
     int windowHeight = 720;
+    boolean nameSet = false;
 
     // Get screen resolution
 //    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 //    double screenWidth = screenSize.getWidth();
 //    double screenHeight = screenSize.getHeight();
-
-    // Pane background color
-    String defaultPaneBGcolor = "ffffff";
 
     // Panes
     Pane pnUsername = new Pane();
@@ -54,33 +54,37 @@ public class UserView extends View {
         // Define button actions
         buttonActions();
 
-        // Image
-        Image image = new Image("File:assets/launcher/bg_name.png");
-        pnUsername.getChildren().add(new ImageView(image));
+        // Background
+        Image usernameBG = new Image("File:assets/launcher/bg_name.png");
+        pnUsername.getChildren().add(new ImageView(usernameBG));
 
         // Name
-        Text username = new Text("Jos Badpak");
-        username.setFont(Font.font("Arial", FontWeight.BOLD, 48));
-        pnUsername.getChildren().add(username);
-        username.setLayoutY(48);
-        username.setLayoutX(64);
+        //Text usernameEdit = new Text("Jos Badpak");
+        usernameEdit.setFont(Font.font("Arial", FontWeight.BOLD, 28));
+        //usernameEdit.setFill(Color.WHITE);
+        pnUsername.getChildren().add(usernameEdit);
+        usernameEdit.setLayoutY(8);
+        usernameEdit.setLayoutX(1600+windowWidth);
+        usernameEdit.setMaxWidth(312);
 
         // Pane username
-        pnUsername.setStyle("-fx-background-color: #"+defaultPaneBGcolor); // Default background color
-        pnUsername.getChildren().add(loginName);
-        loginName.setLayoutX(64);
-        loginName.setLayoutY(32);
-        pnUsername.getChildren().add(loginSubmit);
-        loginSubmit.setLayoutX(64);
-        pnUsername.getChildren().add(logout);
-        logout.setLayoutX(64+128);
+        pnUsername.setStyle("-fx-background-color: #262626"); // Default background color
+
+        // Change name button
+        ImageView usernameEdit = new ImageView("File:assets/launcher/nameEdit.png");
+        pnUsername.getChildren().add(btnChangeName);
+        btnChangeName.setStyle("-fx-background-color: #262626;");
+        btnChangeName.setGraphic(usernameEdit);
+        btnChangeName.setLayoutX(1930+windowWidth);
+        btnChangeName.setLayoutY(8);
 
 
         // Window settings
         stage.setTitle("Epic game launcher");
-        relocatePanes();
         stage.setMinWidth(1024);
         stage.setMinHeight(576);
+        stage.setMaxWidth(2560);
+        stage.setMaxHeight(1440);
         stage.setScene(new Scene(pnUsername, windowWidth, windowHeight));
         stage.show();
 
@@ -97,24 +101,31 @@ public class UserView extends View {
                 relocatePanes();
             }
         });
+
+        // Trigger relocator
+        stage.setWidth(windowWidth+1);
+        stage.setWidth(windowWidth);
     }
 
     private void buttonActions(){
-        // Login button
-        loginSubmit.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                controller.login((loginName.getCharacters().toString()));
-
-            }
-        });
-
         // Logout button
-        logout.setOnAction(new EventHandler<ActionEvent>() {
+        btnChangeName.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                controller.logout();
-                System.out.println("Action has run");
+                if (nameSet == false) {
+                    controller.login((usernameEdit.getCharacters().toString()));
+                    usernameEdit.setDisable(true);
+                    nameSet = true;
+                } else {
+                    int dialogButton = JOptionPane.YES_NO_OPTION;
+                    int dialogResult = JOptionPane.showConfirmDialog (null, "Om uw naam te wijzigen moet u eerst de lobby verlaten. Wilt u de lobby nu verlaten?","Waarschuwing",dialogButton);
+                    if(dialogResult == JOptionPane.YES_OPTION){
+                        // Saving code here
+                        controller.logout();
+                        usernameEdit.setDisable(false);
+                        nameSet = false;
+                    }
+                }
             }
         });
     }
@@ -124,7 +135,7 @@ public class UserView extends View {
     }
 
     private void setUsernamePosition(){
-        pnUsername.setLayoutX(windowWidth-512); // Top right
+        pnUsername.setLayoutX(-3300+windowWidth); // Top right
 
     }
 
